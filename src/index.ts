b14c24c0b7377c8a2fc3ae7a8f51ef4f6e410674
keitagame <keitagame0319@icloud.com>
2025-08-13 13:06:59 +0900

    init

---
+++ src/index.ts
@@ -0,0 +1,7 @@
+export { default as Button } from "./components/Button";
+export { default as Input } from "./components/Input";
+export { default as Onoff } from "./components/Onoff";
+export { default as Card } from "./components/Card";
+export { default as Tabs} from "./components/Tab";
+export { default as TabPane } from "./components/TabPane";
+export { default as Checkbox } from "./components/Check";
